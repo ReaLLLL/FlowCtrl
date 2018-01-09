a4b5d6ddb085fe0dc3aa7b31814185ae63b913ed
Alex Song <songyu@u51.com>
2018-01-09 17:39:50 +0800

    Git Init

---
+++ src/main/java/com/u51/a_little_more/DuoYiDianApplication.java
@@ -0,0 +1,12 @@
+package com.u51.a_little_more;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoYiDianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoYiDianApplication.class, args);
+	}
+}
